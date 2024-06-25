84e67f6634bf6dde925e67589d52cd1fa9b4b5ad
GANESHSAIVENKAT <ganesh043.xworkz@gmail.com>
2024-06-25 19:43:24 +0530

    static array method(25-06-2024)

---
+++ Examination_result.java
@@ -0,0 +1,9 @@
+public class Examination_result {
+    
+     public static int sem_results[] = {90,89,84,96,74,76,82};
+      
+      
+	  
+	 
+     
+}	
---
+++ Examination_result_Runner.java
@@ -0,0 +1,10 @@
+public class Examination_result_Runner{
+	 public static void main(String[] args) {
+		 for (int i = 0; i < Examination_result.sem_results.length; i++) {
+			 
+		    System.out.println("exams follows"+Examination_result.sem_results[i]);  
+	  }
+		System.out.println("The Examination_result of special candidates  are :"+Examination_result.sem_results.length);
+	 }
+
+}
