10f91f4aa67c6b6c42204c2955588effb8e9488b
GANESHSAIVENKAT <ganesh043.xworkz@gmail.com>
2024-06-26 15:51:19 +0530

    25-06-2024

---
+++ Cloths_types.java
@@ -0,0 +1,20 @@
+public class Cloths_types {
+    public static void main(String[] args) {
+        // Declare arrays of different types
+        String[] types_of_cloth = {"Cotton", "Wool", "polymer",};
+        long[] price = {822,500 ,900};
+      
+    
+        for (int i = 0; i < types_of_cloth.length; i++) {
+            System.out.println("types_of_cloth:"+types_of_cloth[i]);
+        }
+
+    
+        for (int i = 0; i < price.length; i++) {
+            System.out.println("price:"+price[i]);
+        }
+
+ 
+    
+    }
+}
