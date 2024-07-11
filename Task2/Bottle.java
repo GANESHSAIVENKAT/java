622c1419cd2b2e703c3fa9937e343db1438402a6
GANESHSAIVENKAT <ganesh043.xworkz@gmail.com>
2024-07-11 23:04:07 +0530

    Revise task

---
+++ Task2/Bottle.java
@@ -0,0 +1,25 @@
+public class Bottle{
+	
+	static String made_in="India";
+	static int quantity=30;
+
+	public static void main(String args[]){
+	String name="MILTA"; 
+	int price=50;
+	float rating=5.5f;
+	double weight=10.2;
+	System.out.println(" Company is:"+name);
+	System.out.println(" Pricedetail is:"+price);
+	System.out.println(" Ratings is:"+rating);
+	System.out.println(" Weight is:"+weight);
+	System.out.println(" Manufactured in:"+made_in);
+	System.out.println(" Bottle Quantity:"+quantity+"ml");
+
+
+	
+	
+	
+	}
+
+
+}
---
+++ Task2/WhatsappRunner.java
@@ -0,0 +1,17 @@
+public class WhatsappRunner{
+	public static void main(String args[]){
+	Whatsapp.info();
+	Whatsapp.users(500000L);
+	Whatsapp.rating((byte)5,500);
+	Whatsapp.grade_info((byte)5,500,'A');
+	Whatsapp.properties((byte)5,500,true,'A');
+	Whatsapp.data("Whatsapp",(byte)5,500,true,'A');
+
+
+
+
+	
+	
+	}
+
+}
